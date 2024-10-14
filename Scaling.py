706382d6e2858e4e0a7d1b34b616a25433214a90
salwamohammed23 <140807017+salwamohammed23@users.noreply.github.com>
2024-10-14 21:23:17 +0300

    Update Scaling.py

--- Scaling.py
+++ Scaling.py
@@ -1,28 +1,25 @@
-
 import numpy as np
 import cv2 as cv
 import matplotlib.pyplot as plt
 import streamlit as st
 
-
-
+# Apply background styles
 page_bg_img = f"""
 <style>
-[data-testid = "stAppViewContainer"]{{
+[data-testid="stAppViewContainer"] {{
     background-image: url("https://img.freepik.com/free-vector/abstract-background_53876-43364.jpg?w=1060&t=st=1685685448~exp=1685686048~hmac=bf06f2136962f77d8fb9a95948390114a68a61622da7713a357e1e359c89618c");
     background-size: cover;
     opacity: 0.9;
-    }}
-[data-testid = "stSidebar"]{{
+}}
+[data-testid="stSidebar"] {{
     background-image: url("https://img.freepik.com/free-vector/multicolor-abstract-background_1123-53.jpg?w=740&t=st=1685685659~exp=1685686259~hmac=d3e48585afea7ba2d11c59452ad8edb5c216e26638437d1de8bbe0dec0188f72");
     background-size: cover;
     opacity: 1;
     filter: blur(0.2px);
-    }}
-
+}}
 </style>
-""" 
-
+"""
+st.markdown(page_bg_img, unsafe_allow_html=True)
 
 # إعداد واجهة المستخدم
 st.title("Image Resizer and Grayscale Converter")
@@ -32,7 +29,7 @@ uploaded_file = st.file_uploader("Upload an Image", type=["jpg", "jpeg", "png"])
 if uploaded_file is not None:
     # قراءة الصورة من الملف المرفوع
     file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
-    img = cv.imdecode(file_bytes, 1)
+    img = cv.imdecode(file_bytes, cv.IMREAD_COLOR)
 
     # عرض الصورة الأصلية
     st.image(img, channels="BGR", caption="Original Image")
@@ -50,15 +47,12 @@ if uploaded_file is not None:
         gray_img = cv.cvtColor(resized_img, cv.COLOR_BGR2GRAY)
 
         # عرض الصورة المعدلة
-        st.image(gray_img, caption="Resized Grayscale Image", use_column_width=True)
-        
-        # إذا أردت عرض الصورة الملونة المعدلة أيضًا
-        # st.image(resized_img, channels="BGR", caption="Resized Color Image")
+        st.image(gray_img, caption="Resized Grayscale Image", channels="GRAY", use_column_width=True)
 
 # عرض تعليمات الاستخدام
 st.markdown("""
 ### Instructions:
-1. Upload an image file.
+1. Upload an image file (JPG or PNG).
 2. Enter the new dimensions for the image.
 3. Click the "Resize and Convert to Grayscale" button to see the resized grayscale image.
 """)
