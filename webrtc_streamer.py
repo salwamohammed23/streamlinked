2f0649b9b87807d5a0ee4bd97bd22b7538457fb5
salwamohammed23 <140807017+salwamohammed23@users.noreply.github.com>
2024-10-17 17:56:13 +0300

    Update webrtc_streamer.py

--- webrtc_streamer.py
+++ webrtc_streamer.py
@@ -1,3 +1,17 @@
+import streamlit as st
 from streamlit_webrtc import webrtc_streamer
+import av
 
-webrtc_streamer(key="sample")
+
+flip = st.checkbox("Flip")
+
+
+def video_frame_callback(frame):
+    img = frame.to_ndarray(format="bgr24")
+
+    flipped = img[::-1,:,:] if flip else img
+
+    return av.VideoFrame.from_ndarray(flipped, format="bgr24")
+
+
+webrtc_streamer(key="example", video_frame_callback=video_frame_callback)
